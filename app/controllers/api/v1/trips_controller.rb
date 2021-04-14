27edf2e4ee65fbebca34748df89b1c908a3df475
Michael Epelboim <sdrmike@outlook.com>
2021-04-14 19:39:47 +0800

    fixed some problems with the trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -5,61 +5,53 @@ class Api::V1::TripsController < Api::V1::BaseController
   # end
 
   def create
-
     @trip = Trip.new(trip_params)
     @trip.status = "open"
+    @trip.user = User.find(params[:user_id])
     if @trip.save
-      @my_trip = calculate_trip()
-      #####function to add the stops
-      
-      render json: { msg: "Created" }
+      @my_trip = calculate_trip
+      @my_trip.each do |id|
+        Trip_stop.create(trip_id: @trip.id, stop_id: id)
+      end
+      render json: { msg: 'Created' }
     else
       render_error(@trip)
     end
-
   end
 
   def calculate_trip
-    start_lat = @trip.start_lan
-    start_lon = @trip.start_lon
-    trip_time_mins = @trip.duration
-    
-    ### for testing only ###
-    start_lat = 31.2288438
-    start_lon = 121.4753802
-    trip_time_mins = 200
+    point_lat = @trip.start_lan
+    point_lon = @trip.start_lon
+    remaining_time_mins = @trip.duration
+
+    #### for testing only ####
+    # start_lat = 31.2288438
+    # start_lon = 121.4753802
+    # remaining_time_mins = 200
     #########################
 
+    # average human walking speed 83 mts / min
     walking_speed = 83
 
     # create the origin stop 
     @stop0 = Stop.new(lat: start_lat, lon: start_lon)
-
-    # current point lat/lon
-    point_lat = start_lat
-    point_lon = start_lon
-    
-    # initialize remaining time variable
-    remaining_time_mins = trip_time_mins
 
     # initialize the trip array - array of instances of stops add origin to the array
     @my_trip = []
     @my_trip << @stop0
     @my_trip_id = []
 
-    puts "*>*>*>*>*>*>*>*>*>*>*>\nremaining time: #{remaining_time_mins}"
-
+    puts "*>*>*>*>*>*>*>*>*>*>*>\noriginal remaining time: #{remaining_time_mins}\n*>*>*>*>*>*>*>*>*>*>*>"
 
     while remaining_time_mins.positive?
-      
+
       # calculate the max distance with the time given
       # 83 mts / min is average walking speed
-      max_distance_mts = walking_speed * remaining_time_mins
-      
+      # max_distance_mts = walking_speed * remaining_time_mins
+
       #### find the array of locations between [start_lat,start_lon] and the max_distance_mts
       # locations = array_of_locs(start_lat, start_lon, max_distance_mts)
       locations = Stop.all
-
 
       ### remove the trip stops already selected for the trip from the locations array
       # ********* this asumes the origin is not a location on our database.
@@ -69,9 +61,8 @@ class Api::V1::TripsController < Api::V1::BaseController
         locations = locations.reject { |location| location.id == stop.id }
       end
 
-
       ### find the distance between my current location and all the points in the array,
-      ### calculate the total_time = walking time to each distace + the visit time. 
+      ### calculate the total_time = walking time to each distace + the visit time.
       ### walking time [mins] = distance [mts] / 83
       locations.each do |location|
         location.total_time = 0
@@ -79,57 +70,43 @@ class Api::V1::TripsController < Api::V1::BaseController
         total_time = (distance_mts / walking_speed) + location.visit_time
         location.total_time = total_time
       end
-      
+
       ### remove the points in the array where the total time is larger than the remaining_time_mins
       small_list = locations.reject { |location| location.total_time > remaining_time_mins.to_f }
-      
-      
+
       ### sort the result by shortest total time
       sorted_locs = small_list.sort_by(&:total_time)
 
-      
-      ### if its more than 10: 
-        ### n = calculate what is the nearest integer to 33% of the size
+      ### if its more than 10: n = calculate what is the nearest integer to 33% of the size
+      ### if its 10 or less: n = the size af the array_of_locs
+      n = sorted_locs.size > 10 ? (sorted_locs.size / 3).ceil : sorted_locs.size
 
-      ### if its 10 or less:
-        ### n = the size af the array_of_locs
-      if sorted_locs.size > 10
-        n = (sorted_locs.size / 3).ceil 
-      else
-        n = sorted_locs.size
-      end
-      
-      
       ### randomly choose between the first n stops
-      puts "sorted locs size = #{n}"
-      puts "sorted locs: #{sorted_locs}"
       if n != 0
         next_stop = sorted_locs[rand(0...n)]
 
         @my_trip << next_stop
         @my_trip_id << next_stop.id
 
+        # set the new location as starting point for the next search
         point_lat = next_stop.lat
         point_lon = next_stop.lon
-        puts "*********************"
+        puts '*********************'
         puts "next stop name: #{next_stop.name}"
       end
-      
+
       remaining_time_mins -= next_stop.total_time
 
       puts "remaining time: #{remaining_time_mins}"
-      puts "*********************"
+      puts '*********************'
     end
-
-    puts @my_trip
-    puts @my_trip_id
-    return @my_trip_id
-    
+    @my_trip_id
   end
-  
+
   private
-    def trip_params
-      params.require(:trip).permit(:duration, :start_lat, :start_lon, :status)
-    end
+
+  def trip_params
+    params.require(:trip).permit(:duration, :start_lat, :start_lon, :status, :user_id)
+  end
   
 end
